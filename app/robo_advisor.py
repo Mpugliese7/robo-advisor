5ed44d4981493a87afcd90d530d4cef724aa0b3b
Mpugliese7 <65572796+Mpugliese7@users.noreply.github.com>
2020-06-13 13:45:10 -0400

    dynamically get latest day's close

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -21,7 +21,10 @@ parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
-latest_close = parsed_response["Time Series (Daily)"]["2020-06-12"]["4. close"]
+date_keys = parsed_response["Time Series (Daily)"].keys()
+dates = list(date_keys) # assumes first day is on top
+latest_day = dates[0]
+latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
 
 #breakpoint()
 
